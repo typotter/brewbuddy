5658e493a1ae0511f3e83551e3e65f7ab43f51b5
Ty Potter <tyler.john.potter@gmail.com>
2017-08-31 18:50:36 -0600

    Initial commit with working chrome extension/firebase skeleton

---
+++ background.js
@@ -0,0 +1,39 @@
+var config = {
+  apiKey: "AIzaSyCfHp6dkivD_7EUQyiCn3ulSoJo5L_qoE8",
+  databaseURL: "https://brewconsole.firebaseio.com",
+  storageBucket: "brewconsole.appspot.com",
+};
+var fb = firebase.initializeApp(config);
+
+/**
+ * initApp handles setting up the Firebase context and registering
+ * callbacks for the auth status.
+ *
+ * The core initialization is in firebase.App - this is the glue class
+ * which stores configuration. We provide an app name here to allow
+ * distinguishing multiple app instances.
+ *
+ * This method also registers a listener with firebase.auth().onAuthStateChanged.
+ * This listener is called when the user is signed in or out, and that
+ * is where we update the UI.
+ *
+ * When signed in, we also authenticate to the Firebase Realtime Database.
+ */
+function initApp() {
+  // Listen for auth state changes.
+  firebase.auth().onAuthStateChanged(function(user) {
+    console.log('User state change detected from the Background script of the Chrome Extension:', user);
+  });
+}
+
+window.onload = function() {
+  initApp();
+};
+
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+  console.log("Received %o from %o, frame", msg, sender.tab, sender.frameId);
+  if (msg.method == "GET") {
+    firebase.database().ref(msg.path).once('value', sendResponse);
+  }
+  return true;  // Indicates Async resolution
+});
---
+++ content_script.js
@@ -0,0 +1,139 @@
+console.log("content script running");
+/*
+chrome.runtime.sendMessage({text: "hey"}, function(response) {
+    console.log("Response: ", response);
+});
+*/
+
+var injectScript = function(func) {
+  var actualCode = '(' + func + ')();';
+  var script = document.createElement('script');
+  script.textContent = actualCode;
+  (document.head||document.documentElement).appendChild(script);
+  script.remove();
+}
+
+var afterViewRecord = function(ele) {
+  console.log('parsing page');
+  var overlay = matchPageToOverlay();
+  if (overlay != null) {
+    overlay();
+  } else {
+    console.log('no page overlay');
+  }
+  
+}
+
+var matchPageToOverlay = function() {
+  if ($('div.formTitle:contains("Open Batches")').length > 0){
+    return batchPageOverlay;
+  }
+  return null;
+}
+
+
+_FIELD_MAP = {
+  "batch.batch_id": "Batch Number",
+  "batch.product": "Product",
+  "batch.recipe": "Recipe",
+  "batch.status": "Status"};
+
+var getEkosFieldId = function(fieldId, domRoot) {
+  return $('div.inputfieldlabel:has(> label:contains("' + _FIELD_MAP[fieldId] + '"))', domRoot).attr('class').match(/(\d+)Input/)[1];
+}
+
+var getEkosFieldValue = function(id, domRoot) {
+  return $('.' + id + 'InputValue', domRoot).text().trim();
+}
+
+var batchPageOverlay = function() {
+  console.log('batch page overlay');
+
+  var domRoot = $('#formFrame_0')[0];
+
+  var loaded = false;
+  $(domRoot).load(function() {
+    if (!loaded) {
+      loaded = true;
+      loadBatchPageOverlay(domRoot.contentDocument);
+    }
+
+  });
+}
+
+var loadBatchPageOverlay = function(domRoot) {
+  console.log('loading page overlay.');
+  batchId = getEkosFieldValue(getEkosFieldId('batch.batch_id', domRoot), domRoot);
+
+  console.log("Batch ID: " + batchId);
+
+  //console.log(firebase.auth());
+
+  /*firebase.database().ref('/batches/' + batchId).once('value').then(function(snapshot) {
+    console.log('batch read returned.');
+    console.log(snapshot.val());
+
+    
+
+  });*/
+batchId = "PB0060";
+  chrome.runtime.sendMessage({method: "GET", path: '/batches/' + batchId}, function(response) {
+      console.log("Response: ");
+      console.log(response);
+  });
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var injectHooks = function() {
+  var _oldViewRecord = viewRecord;
+  console.log('hooking onto viewRecord.');
+  viewRecord = function(args) {
+    _oldViewRecord.apply(this, arguments);
+    console.log('dispatching after view record.');
+    var event = new Event('afterViewRecord');
+    document.body.dispatchEvent(event);
+  }
+
+  // For development
+  viewRecord('753','56','',window,'','','','','','','','');
+}
+
+console.log('attaching listeners');
+document.body.addEventListener('afterViewRecord', afterViewRecord, false);
+
+console.log('injecting functions');
+injectScript(injectHooks);
+
+
+// Inform the background page that 
+// this tab should have a page-action
+chrome.runtime.sendMessage({
+  from:    'content',
+  subject: 'showPageAction'
+});
+
+
+var config = {
+  apiKey: "AIzaSyCfHp6dkivD_7EUQyiCn3ulSoJo5L_qoE8",
+  databaseURL: "https://brewconsole.firebaseio.com",
+  storageBucket: "brewconsole.appspot.com",
+};
